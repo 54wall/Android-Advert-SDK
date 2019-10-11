a88cffccac5be11e3f35fa9d21bdccc69f3fe765
54wall <weiqiang_1989@126.com>
2019-10-11 18:35:40 +0800

    增加视频播放

--- advertlibrary/src/main/java/advert/sdk/com/advertlibrary/ui/SecondaryPresentation.java
+++ advertlibrary/src/main/java/advert/sdk/com/advertlibrary/ui/SecondaryPresentation.java
@@ -2,10 +2,18 @@ package advert.sdk.com.advertlibrary.ui;
 
 import android.app.Presentation;
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Button;
 import android.widget.ImageView;
 
+
+import java.io.IOException;
 
 import advert.sdk.com.advertlibrary.R;
 
@@ -15,13 +23,18 @@ import advert.sdk.com.advertlibrary.R;
 public class SecondaryPresentation extends Presentation {
 
     private ImageView mIvAd;
-
+    private SurfaceView surfaceview;
+    private MediaPlayer mediaPlayer;
+    private int postion = 0;
+    private Context context;
     public SecondaryPresentation(Context context, Display display) {
         super(context, display);
+        this.context = context;
     }
 
     public SecondaryPresentation(Context context, Display display, int theme) {
         super(context, display, theme);
+        this.context = context;
     }
 
     @Override
@@ -29,11 +42,76 @@ public class SecondaryPresentation extends Presentation {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.presentation_secondary);
         mIvAd = findViewById(R.id.iv_ad);
+        surfaceview = (SurfaceView) findViewById(R.id.surfaceView);
+        initView();
 
     }
 
     public void setAd(int res) {
         mIvAd.setImageResource(res);
+    }
+
+    protected void initView() {
+        // TODO Auto-generated method stub
+        mediaPlayer = new MediaPlayer();
+        surfaceview.getHolder().setKeepScreenOn(true);
+        surfaceview.getHolder().addCallback(new SurfaceViewLis());
+//        btnGo.setOnClickListener(this);
+    }
+
+    private class SurfaceViewLis implements SurfaceHolder.Callback {
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                                   int height) {
+
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            if (postion == 0) {
+                try {
+                    play();
+                } catch (IllegalArgumentException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (SecurityException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IllegalStateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
+
+    }
+
+    public void play() throws IllegalArgumentException, SecurityException,
+            IllegalStateException, IOException {
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        AssetFileDescriptor fd = context.getAssets().openFd("start.mp4");
+        mediaPlayer.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(),
+                fd.getLength());
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setDisplay(surfaceview.getHolder());
+        // 通过异步的方式装载媒体资源
+        mediaPlayer.prepareAsync();
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                // 装载完毕回调
+                mediaPlayer.start();
+            }
+        });
     }
 
 
